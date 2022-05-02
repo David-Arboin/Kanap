b0844555de43c0309b5256770c794ff3c373ba14
david-arboin <david.arboin@gmail.com>
2022-05-02 07:03:46 +0200

    Mise à jour et affichage du nombre de canapé(s) dans le panier à l'ouverture et au rafraichissement d'une page produit à côté du mot panier dans le Header

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,36 @@
+//--Utilisation de l'interface URLSearchParams qui permet de travailler avec l'URL de la page active
 const urlSearchParams = new URLSearchParams(window.location.search);
 console.log(urlSearchParams);
 
+//--Récupérartion de l'id du produit
 const idProduct = urlSearchParams.get("id");
 console.log(idProduct);
 
+//--Informations du panier : Quantité affichée à côté du mot panier dans le Header
+let updateNumberProductInCart = () => {
+
+let productsInCart = JSON.parse(localStorage.getItem("cart"))
+console.log(productsInCart)
+let numberProductInCart = 0
+for (i = 0; i < productsInCart.length; i++) {
+    if (productsInCart[i].color != "" && productsInCart[i].quantity > 0) {
+        numberProductInCart = parseInt(numberProductInCart) + parseInt(productsInCart[i].quantity)
+        console.log(numberProductInCart)
+     }
+}
+//--Affichage du nombre de canapés dans le panier au moment de l'ouverture de la apge
+let displayUpdateCart = () => {
+    let sectionCart = document.getElementsByTagName("ul")[1];
+    let updateInfoCart = document.createElement("p");
+    console.log(updateInfoCart)
+    sectionCart.appendChild(updateInfoCart);
+    updateInfoCart.innerHTML = `: ${numberProductInCart} canapés`;
+    }
+    displayUpdateCart()
+}
+updateNumberProductInCart()
+
+//--Appel des données liées au produits
 const fetchProduct = async () => {
     await fetch(`http://localhost:3000/api/products/${idProduct}`)
     .then((res) => res.json())
@@ -13,7 +40,7 @@ const fetchProduct = async () => {
     })
 };
 
-//Affichage des détails du produit
+//--Affichage des détails du produit
 const displayPageProduct = async () => {
     await fetchProduct();
 
@@ -22,7 +49,7 @@ const displayPageProduct = async () => {
     let priceProduct = document.getElementById("price").innerText = `${productData.price}`
     let descriptionProduct = document.getElementById("description").innerText = `${productData.description}`
 
-//Voir dans la console le nombre de couleur possible
+//--Optionnel--Voir dans la console le nombre de couleur possible
     console.log(productData.colors.length)
 
 //Boucle de création des choix de couleurs
@@ -34,15 +61,15 @@ const displayPageProduct = async () => {
         optionColors.value = `${productData.colors[i]}`
     }
 }
-displayPageProduct();
+displayPageProduct(); //--Exécution de la fonction displayPageProduct
 
 //Ecoute du click sur Ajouter au panier
 document.getElementById("addToCart").addEventListener("click", addToCart)
 
-//---Déclration de la fonction de remplissage du locale storage après insertion du premier produit
+//---Déclration de la fonction de remplissage du panier
 function addToCart() {//addToCart
 
-//---Récupération des données de la page produit pour les envoyers dans le panier
+//---Récupération des données de la page produit pour les envoyers dans le panier s'il n'y est pas déjà
     let newProduct = {
     _id : productData._id, 
     color : document.getElementById("colors").value, 
@@ -51,38 +78,62 @@ function addToCart() {//addToCart
     alt : productData.altTxt,
     name : productData.name
 }
+
+//--Si la couleur ou la couleur n'est pas choisie, le client est avertit
+if (newProduct.color === ""){
+    document.getElementById("colors").style.backgroundColor = "green"
+}
+if (newProduct.color != ""){
+    document.getElementById("colors").style.backgroundColor = "#3B3B3B"
+}
+if (newProduct.quantity == 0){
+    document.getElementById("quantity").style.backgroundColor = "green"
+}
+if (newProduct.quantity > 0){
+    document.getElementById("quantity").style.backgroundColor = "#3B3B3B"
+}
+//--Optionnel--Permet de voir les données du produit de la page active même la quantité saisie
 console.log(newProduct)
 
-//Consulter le Local Storage pour savoir s'il contient déjà des produits
+//--Récupérer le contenu du panier
     let productsInCart = JSON.parse(localStorage.getItem("cart"))
     console.log(newProduct)
     console.log(productsInCart)
-//---Premier produit dans le local storage
 
-function addNewProduct () {
-    const isProductAlreadyPresent = productsInCart.some(product => productsInCart._id === newProduct._id && productsInCart.color === newProduct.color)
-    console.log(isProductAlreadyPresent)
-}
-
+//---Premier produit dans le panier s'il n'y en a pas déjà un
 if (productsInCart == null) {
-    let productsInCart = []
+    productsInCart = []
     productsInCart.push(newProduct)
     console.log(productsInCart) 
     localStorage.setItem("cart", JSON.stringify(productsInCart))  
 }
-else if (isProductAlreadyPresent){
-    productsInCart.map(productsInCart => {
-        if (productsInCart._id === newProduct._id && productsInCart.color === newProduct.color){
-            productsInCart.quantity = newProduct.quantity
-            localStorage.setItem("cart", JSON.stringify(productsInCart))  
-            console.log("ok")
-        }
-        return productsInCart;
-    })
-}
-else {
+//--Si le panier contient un produit identique, on met sa quantité à jour
+else if (productsInCart.some(product => product._id === newProduct._id && product.color === newProduct.color)){
+        console.log(productsInCart.some(product => product._id === newProduct._id && product.color === newProduct.color))
+        productsInCart.map(product => { 
+            if (product._id === newProduct._id && product.color === newProduct.color) {
+                product.quantity = newProduct.quantity
+                localStorage.setItem("cart", JSON.stringify(productsInCart))  
+            }
+            return product
+})
+//--Si le produit est nouveau, on l'ajoute
+}    else {
     productsInCart.push(newProduct)
     console.log(productsInCart) 
     localStorage.setItem("cart", JSON.stringify(productsInCart))  
 }
+//--Mise à jour des informations du panier : Quantité affichée à côté du mot panier dans le Header
+let updateNumberProductInCartAfterClick = () => {
+
+    let numberProductInCartAfterClick = 0
+    for (i = 0; i < productsInCart.length; i++) {
+        if (productsInCart[i].color != "" && productsInCart[i].quantity > 0) {
+            numberProductInCart = parseInt(numberProductInCart) + parseInt(productsInCart[i].quantity)
+            console.log(numberProductInCart)
+         }
+         return numberProductInCart
+    }
+    }
+    updateNumberProductInCartAfterClick()
 }
